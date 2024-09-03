a73af0e8e943827fa6fc305c68aa88e797c4ea48
Leon <richardleonardo1234@gmail.com>
2024-09-03 16:01:17 -0400

    DecisãoIf_C#

---
+++ decisaoIf_C#/Program.cs
@@ -0,0 +1,37 @@
+﻿//Exercicios Decisão C#//
+
+
+//1) Entrar com um número e imprimir uma das mensagens: é múltiplo de 5 ou não é múltiplo de 5.//
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 5 == 0)
+        {
+            Console.WriteLine($"O número {numero} é múltiplo de 5.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {numero} não é múltiplo de 5.");
+        }
+    }
+}
+
+
+
+//2)//
+//3)//
+//4)//
+//5)//
+//6)//
+//7)//
+//8)//
+//9)//
+//10)//
+//11)//
+//12)//
+//13)//
